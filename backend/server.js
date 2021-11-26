e0d166de954268ae7576daae10bb119487a33926
tk04 <turkisaloufi@gmail.com>
2021-11-25 19:51:23 -0500

    added backend endpoints, created database, and connected to frontend

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+import express from "express"
+import dotenv from "dotenv"
+import connectDB from "./config/db.js"
+import colors from "colors"
+import productRoutes from "./routes/productRoutes.js"
+import {notFound, errorHandler} from "./middleware/errorMiddleware.js"
+
+dotenv.config()
+
+connectDB()
+const app = express()
+
+app.use("/api/products", productRoutes)
+app.use(notFound)
+app.use(errorHandler)
+app.get("/", (req,res) => {
+    res.send("API is running")
+})
+
+
+const PORT = process.env.PORT || 4000
+app.listen(PORT, () => console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold))
