c10c9e57137875ba6315945f667359f06fd1bfdd
tk04 <turkisaloufi@gmail.com>
2021-11-28 14:03:09 -0500

    added product screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,6 +14,7 @@ import PlaceOrderScreen from "./screens/PlaceOrderScreen";
 import OrderScreen from "./screens/OrderScreen";
 import UserListScreen from "./screens/UserListScreen";
 import UserEditScreen from "./screens/UserEditScreen";
+import ProductListScreen from "./screens/ProductListScreen";
 
 const App = () => {
   return (
@@ -32,6 +33,7 @@ const App = () => {
           <Route path="/cart/:id?" component={CartScreen}  />
           <Route path="/" component={HomeScreen} exact />
           <Route path="/admin/userList" component={UserListScreen} exact />
+          <Route path="/admin/productList" component={ProductListScreen} exact />
           <Route path="/admin/user/:id/edit" component={UserEditScreen} eact />
       </Container>
     </main>
