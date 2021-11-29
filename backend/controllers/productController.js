1d10e17bed4373a0eb9b6c0934f9d52f4f69c064
tk04 <turkisaloufi@gmail.com>
2021-11-28 20:43:48 -0500

    integrated product review frontend + backend endpoints

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -78,11 +78,44 @@ const updateProduct = AsyncHandler(async (req,res) => {
   
 })
 
+
+// @desc create new review
+// @route POST /api/products/:id/reviews
+// @access Public
+const createProductReview = AsyncHandler(async (req,res) => {
+    const product = await Product.findById(req.params.id)
+    if(!product){
+        res.status(404)
+        throw new Error("Product not found")
+    }
+    const {rating, comment} = req.body
+    const alreadyReviewd = product.reviews.find(r => r.user.toString() === req.user._id.toString())
+    if(alreadyReviewd){
+        res.status(400)
+        throw new Error("Product already reviewed")
+    }
+    const review = {
+        name: req.user.name,
+        rating: Number(rating),
+        comment: comment,
+        user: req.user._id
+    }
+    product.reviews.push(review)
+    product.numReviews = product.reviews.length
+
+    product.rating = product.reviews.reduce((acc, item) => item.rating + acc, 0) / product.reviews.length
+
+    await product.save()
+    res.status(201).json({message: "Review added"})
+  
+})
+
 export {
     getProducts,
     getProductById,
     deleteProduct,
     createProduct,
-    updateProduct
+    updateProduct,
+    createProductReview
 
 }
--- backend/server.js
+++ backend/server.js
@@ -8,11 +8,16 @@ import orderRoutes from "./routes/orderRoutes.js"
 import {notFound, errorHandler} from "./middleware/errorMiddleware.js"
 import uploadRoutes from "./routes/uploadRoutes.js"
 import path from "path"
+import morgan from "morgan"
 
 dotenv.config()
 
 connectDB()
 const app = express()
+
+if(process.env.NODE_ENV === "development"){
+    app.use(morgan('dev'))
+}
 
 app.use(express.json())
 
